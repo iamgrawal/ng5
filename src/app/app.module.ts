70e425f189de3ec2176fa22d3da4e84f3de3bbb1
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-01-11 01:21:04 +0530

    Interpolations Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -15,6 +16,7 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  itemCount: number;
+  btnText: string;
+  goalText: string;
+  goals = [];
+
+
+  constructor() {
+    this.itemCount = 4;
+    this.btnText = 'Add an Item';
+    this.goalText= 'My first life goal';
+   }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
