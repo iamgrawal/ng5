0bf17357c71f449779227a78ba08655951bf6067
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-01-11 10:50:22 +0530

    Animations Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +17,7 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule
